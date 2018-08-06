ae87a46825c34234c1c49f6d4947f624de828759
Magno-D <40536442+Magno-D@users.noreply.github.com>
2018-08-06 21:36:56 +0300

    Update mk.js

--- mk.js
+++ mk.js
@@ -275,8 +275,8 @@ bot.on("message", async message => {
 bot.on("ready", function() {
 	console.log("ready");
 	
-	bot.user.setStatus("Online");
-	bot.user.setGame("To Get Help | 1help");
+	bot.user.setStatus("dnd");
+	bot.user.setGame("To Get Help | 3help");
 });
 bot.on('message', message => {
 if (message.content === prefix +'help') {
@@ -284,29 +284,29 @@ if (message.content === prefix +'help') {
          let embed = new Discord.RichEmbed()
 .setThumbnail(message.author.avatarURL)
 	.setDescription (`
-#User "1userinfo [username or blank]": displays info for the selected user or for message sender if arguments are not provided
+#User "3userinfo [username or blank]": displays info for the selected user or for message sender if arguments are not provided
 
-#Music "1play [song name or blank]": plays a song or if arguments are not provided plays the first song in the queue
+#Music "3play [song name or blank]": plays a song or if arguments are not provided plays the first song in the queue
 
-"1skip": skips the current song
+"3skip": skips the current song
 
-"1queue": displays current queue
+"3queue": displays current queue
 
-"1queue remove [song number]": removes chosen song from the queue
+"3queue remove [song number]": removes chosen song from the queue
 
-"1queue clear": removes all songs from the queue
+"3queue clear": removes all songs from the queue
 
-"1queue shuffle": shuffles the current queue
+"3queue shuffle": shuffles the current queue
 
-"1repeat": plays current song again
+"3repeat": plays current song again
 
-"1stop": stops playing music and deletes all songs in the queue
+"3stop": stops playing music and deletes all songs in the queue
 
-"1yt [search term]": searches the YouTube and returns first 5 results
+"3yt [search term]": searches the YouTube and returns first 5 results
 
-"1add [search result number]": adds a song from YouTube search to the queue
+"3add [search result number]": adds a song from YouTube search to the queue
 
-"1vol [percentage]": sets the volume of the music to given percentage
+"3vol [percentage]": sets the volume of the music to given percentage
 
 ● ▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬ ● 
 `)
